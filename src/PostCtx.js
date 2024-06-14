af954097ddcbfd58d7836b46afbd562ba792a8b0
Mehdi-njfn <najafianmehdi2000@gmail.com>
2024-06-14 11:40:41 +0330

    post ctx

---
+++ src/PostCtx.js
@@ -0,0 +1,55 @@
+import { faker } from "@faker-js/faker";
+import { createContext, useContext, useState } from "react";
+
+const PostCtx = createContext();
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+  return (
+    <PostCtx.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostCtx.Provider>
+  );
+}
+function usePost() {
+  const contaxt = useContext(PostCtx);
+  if (contaxt === undefined)
+    throw new Error("PostContaxt was be used outside of PostProvider");
+  return contaxt;
+}
+export { PostProvider, usePost };
